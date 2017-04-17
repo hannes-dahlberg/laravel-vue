ad993bdd46e5da8d87d8796823ebb8a68d4b2899
Hannes Dahlberg <hannes@dwik.se>
2017-04-17 18:46:25 +0200

    Authentication controller for API authentication using passport. Set passport password client secret in .env

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,9 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+Route::group(['namespace' => 'API'], function() {
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::get('auth/user', ['name' => 'auth.user', 'uses' => 'AuthController@getUser']);
+    });
+    Route::post('auth', ['name' => 'auth', 'uses' => 'AuthController@Auth']);
 });
