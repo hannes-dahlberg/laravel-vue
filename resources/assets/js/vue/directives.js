7cae48bcd064ea5cb82e7943f7f6426c5a834819
Hannes Dahlberg <hannes@dwik.se>
2017-05-04 15:30:37 +0200

    Parallax scrolling directive

--- resources/assets/js/vue/directives.js
+++ resources/assets/js/vue/directives.js
@@ -7,3 +7,10 @@ Vue.directive('input-focus', function (el, value) {
 Vue.directive('tooltip', function(el, binding) {
     $(el).tooltip(binding.value)
 })
+
+Vue.directive('parallax', (el, binding) => {
+    if(!binding.value) { return }
+    this.scrollEvent = $(window).scroll((e) => {
+        $(el).css('background-position-y',  ($(window).scrollTop() - $(el).offset().top) * binding.value.speed)
+    })
+})
