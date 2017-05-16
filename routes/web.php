4ed683e9203b984816c5177db1ed95949997a397
Hannes Dahlberg <hannes@dwik.se>
2017-05-16 15:36:51 +0200

    Fixed bug when visiting root url

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 //Catch all request except under the API prefix and render index view
-Route::any('{catchall}', function($url) {
+Route::any('{catchall}', function() {
     return view('index');
 })->where('catchall', '(?:(?!api).)*');
 
