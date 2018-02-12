f8e31e49c6b511d738d2d807f242ead3a3186068
Hannes Dahlberg <hannes@dwik.se>
2018-02-12 10:57:59 +0100

    Added typescript support and implemented auth vuex store module to use it

--- resources/assets/js/vue/store/modules/auth.ts
+++ resources/assets/js/vue/store/modules/auth.ts
@@ -1,48 +1,58 @@
-import auth from '../../../api/auth'
+import {Auth, authPayload} from '../../../api/auth'
+
+declare var axios: any
+declare var Echo: any
+declare var window: any
+
+type state = {
+    login: boolean,
+    user: string,
+    token: string
+}
 
 export default {
-    state: {
-        login: false,
-        user: null,
-        token: null
+    state: <state>{
+        login: <boolean>false,
+        user: <string>null,
+        token: <string>null
     },
     mutations: {
-        setAuth(state, payload) {
+        setAuth(state: state, payload: { user: string, token: string }) {
             state.login = true
             state.user = payload.user
             state.token = payload.token
         },
-        setLogoutState(state) {
+        setLogoutState(state: state) {
             state.login = false
             state.user = null
             state.token = null
         }
     },
     actions: {
-        auth({ commit, dispatch }, payload) {
+        auth({ commit, dispatch }: { commit: any, dispatch: any }, payload: authPayload) {
             return new Promise((resolve, reject) => {
-                auth.auth(payload).then(response => {
+                Auth.auth(payload).then((response: any) => {
                     //Call to set auth values
                     commit('setAuth', { user: response.user, token: response.token['access_token'] })
                     dispatch('addTokenToHeaders')
 
                     resolve()
-                }).catch(error => reject(error))
+                }).catch((error: Error) => reject(error))
             })
         },
-        socialAuth({ commit, dispatch }, routeQuery) {
+        socialAuth({ commit, dispatch }: { commit: any, dispatch: any}, routeQuery: { driver: string, code: string }) {
             if(!routeQuery.driver) { return }
             return new Promise((resolve, reject) => {
-                auth.socialAuth({ url: routeQuery.driver + '/callback?code=' + routeQuery.code }).then(response => {
+                Auth.socialAuth(routeQuery.driver + '/callback?code=' + routeQuery.code).then((response: any) => {
                     //Call to set auth values
                     commit('setAuth', { user: response.user, token: response.token })
                     dispatch('addTokenToHeaders')
 
                     resolve()
-                }).catch(error => reject(error))
+                }).catch((error: Error) => reject(error))
             })
         },
-        addTokenToHeaders({ getters }) {
+        addTokenToHeaders({ getters }: { getters: any }) {
             if(!getters.getToken) { return; }
             //Setting the Echo header to include token needed when listening on private channels
             if(window.Echo) {
@@ -52,18 +62,18 @@ export default {
             //Setting axios default header to include token for any future requests
             axios.defaults.headers.common['Authorization'] = 'Bearer ' + getters.getToken
         },
-        logout({ commit }) {
+        logout({ commit }: { commit: any }) {
             commit('setLogoutState')
         }
     },
     getters: {
-        isAuth: state => {
+        isAuth: (state: state) => {
             return state.login
         },
-        getUser: state => {
+        getUser: (state: state) => {
             return state.login ? state.user : null
         },
-        getToken: state => {
+        getToken: (state: state) => {
             return state.login ? state.token : null
         }
     }
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ let mix = require('laravel-mix')
 let webpack = require('webpack')
 let Dotenv = require('dotenv-webpack')
 
-mix.js('resources/assets/js/app.js', 'public/js')
+mix.ts('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
     .webpackConfig({
         plugins: [
