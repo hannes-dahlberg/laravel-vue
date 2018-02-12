79ae433309ef15c0d640353f34ae424368642e8c
Hannes Dahlberg <hannes@dwik.se>
2018-02-12 12:58:54 +0100

    Mix manifest should be a part of source and deploy script uses .env all the time

--- deploy.js
+++ deploy.js
@@ -1,5 +1,5 @@
 require('dotenv').config({
-    path: './.env' + (process.env.NODE_ENV != 'development' ? '.' + process.env.NODE_ENV : '')
+    path: './.env'
 })
 
 var exec = require('child_process').exec;
