78e9ebc67bee6a5067f969cd823dde4c609af59f
Hannes Dahlberg <hannes@dwik.se>
2018-02-12 15:30:09 +0100

    Collapsing navbar on click

--- resources/assets/js/vue/templates/components/navbar/navbar.vue
+++ resources/assets/js/vue/templates/components/navbar/navbar.vue
@@ -2,12 +2,12 @@
     <ul class="navbar-nav" :class="{ 'ml-auto': right, 'mr-auto': !right }">
         <li class="nav-item px-2 align-self-end align-self-lg-center" v-for="item in menu" :class="{ 'dropdown': item.menu, 'divider': item.divider, 'd-block, d-lg-none': item.mobileOnly, 'd-none d-lg-block': item.desktopOnly, 'active': $route.name == item.route || activeSubMenu(item) }">
             <a v-if="!item.menu && !item.divider && item.url" class="nav-link" :href="item.url" v-html="$t(item.name)"></a>
-            <router-link v-else-if="!item.menu && !item.divider" class="nav-link" :to="item.route" v-html="$t(item.name)"></router-link>
+            <router-link v-else-if="!item.menu && !item.divider" v-on:click.native="collapse" class="nav-link" :to="item.route" v-html="$t(item.name)"></router-link>
             <a v-if="item.menu" class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-html="$t(item.name)"></a>
             <div v-if="item.menu" class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                 <div v-for="item in item.menu" :class="{ 'divider': item.divider, 'd-block, d-lg-none': item.mobileOnly, 'd-none d-lg-block': item.desktopOnly }">
                     <a v-if="!item.divider && item.url" class="dropdown-item" :class="{ 'active': $route.name == item.route }" :href="item.url" v-html="$t(item.name)"></a>
-                    <router-link v-else-if="!item.divider" class="dropdown-item" :class="{ 'active': $route.name == item.route }" :to="item.route" v-html="$t(item.name)"></router-link>
+                    <router-link v-else-if="!item.divider" v-on:click.native="collapse" class="dropdown-item" :class="{ 'active': $route.name == item.route }" :to="item.route" v-html="$t(item.name)"></router-link>
                 </div>
             </div>
         </li>
@@ -21,9 +21,11 @@
             activeSubMenu(menuItem) {
                 //No menu exists, return false
                 if(!menuItem.menu) { return false }
-
                 //Active if a submenu child is the current route
                 return menuItem.menu.findIndex(subMenu => this.$route.name == subMenu.route) != -1 ? true : false
+            },
+            collapse() {
+                    $('.navbar-collapse').collapse('hide')
             }
         }
     }
